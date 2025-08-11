a8a1490644d5eb589e85fc66ced5c5f73fb0a968
Damien Oriente <damiodev@icloud.com>
2025-08-11 06:27:01 +0200

    feat: trigger new deployment with terser fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// App principale de DevWeb31 - Version production
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Header } from './components/Layout/Header'
 import { Footer } from './components/Layout/Footer'
